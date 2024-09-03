886f089df3745d64098c1dcf1f09b93ca10d23ff
avelana <o.g.kazantseva@gmail.com>
2024-09-04 02:02:32 +0300

    input-output of data in the program "hello world" in encodings 1251 and utf-8

--- 01-linear-algorithms/ex-01-hello-world-ru/main.cpp
+++ 01-linear-algorithms/ex-01-hello-world-ru/main.cpp
@@ -1,20 +1,20 @@
 ﻿# include  <iostream> 
 
-
-// 65001 - это консольная кодовая страница для UTF-8.
-
+// Для автоматического сохранения в UTF-8 всех новых файлов, содержащих символы, 
+// отличные от набора ASCII, необходимо включить опцию
+// - Tools > Options > Environment > Documents > Save documents as Unicode when data cannot be saved in codepage
 
 int  main() {
-    //setlocale(LC_ALL, "Russian");
+   
+    setlocale(LC_ALL, "Russian");
     system("chcp 1251"); // настраиваем кодировку консоли
-    //      chcp изменяет кодировку только консольного вывода, а ввод остаётся в кодировке 1251
-
-    //system("chcp 65001"); // настраиваем кодировку консоли
-
+   
     std::cout << "Компьютер: Привет, мир!" << std::endl;
     std::cout << "Человек, как тебя зовут?" << std::endl;
 
     std::string name;
     std::cin >> name;
     std::cout << "Привет, " << name << "!";
+    
+    return 0;
 }
